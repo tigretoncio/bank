61b20dc95cc39faac4a33a9bba86cb62b374daef
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-06-28 11:40:15 +0100

    second test, add deposits

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,11 @@ describe Account do
       expect(account.balance).to eq 0
     end
   end
+
+  context "adding deposits" do
+    it "when deposit of 2000, balance increases by 2000" do
+      account.deposit(2000)
+      expect(change{account.balance}.by(2000))
+    end
+  end
 end
