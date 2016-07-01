9ac147b2b8c73e1deb75ab163c6dcec2137768c5
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-07-01 17:07:39 +0100

    WIP add filtering capabilities

--- lib/print_statement.rb
+++ lib/print_statement.rb
@@ -27,9 +27,13 @@ class PrintStatement
     def filter_sort(type)
       case type
       when "deposits"
+        return @statements.find_all {|t| t.type == "deposit"}
       when "withdraws"
+        return @statements.find_all {|t| t.type == "witdraw"}
       when "asc"
+        return @statements.sort_by {|t| t.date}
       when "desc"
+        return @statements.sort_by {|t| t.date}.reverse
       end
     end
 
